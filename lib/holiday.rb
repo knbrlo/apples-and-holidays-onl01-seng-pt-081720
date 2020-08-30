3d7ef3ea680ac1461ae5e73ce7534847e6588d85
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 02:12:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
 
     #Season: todo PUTS THIS
-    season = key.to_s.capitalize << ":"
+    puts season = key.to_s.capitalize << ":"
     value.each do |holiday, supply|
 
       #Holiday: todo PUTS THIS
