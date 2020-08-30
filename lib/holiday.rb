47125228d6d4e590c859ee8f2c7f46bddd792863
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 02:18:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
       #Holiday: todo PUTS THIS
       holiday_name = "  " << holiday.to_s.capitalize << ": "
 
-      xxc = supply.array.join(',')
+      xxc = supply.join(',')
       binding.pry
 
       supply.each do |item|
