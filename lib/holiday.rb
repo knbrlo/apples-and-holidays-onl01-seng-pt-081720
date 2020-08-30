4bd9197a6595f5d266b9ded89c0f68d07fe9f603
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 01:30:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 
 
 
-  
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -68,10 +68,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
