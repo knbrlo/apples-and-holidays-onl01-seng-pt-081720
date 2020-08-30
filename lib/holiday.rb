2d809574eb9550683d3a57e0d4b9d893843a4a2e
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 02:36:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,10 +88,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  final_array = []
   holiday_hash.each do |key, value|
     value.each do |holiday, supplies|
-      puts supplies.include?("BBQ")
+        if supplies.include?("BBQ")
+          final_array << holiday
+        end
     end
   end
-
+  final_array
 end
