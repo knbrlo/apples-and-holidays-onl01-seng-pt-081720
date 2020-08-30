943fe90b9eb95cac657c709b6117bd75529ea7c7
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 01:49:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    key.to_s.capitalize << ":"
+    season = key.to_s.capitalize << ":"
     binding.pry
   end
 end
